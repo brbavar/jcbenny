3f70e2e444455bfe1a58cd3d4b54dbcd7216810a
Ben Bavar <ben.bavar@gmail.com>
2023-10-18 23:02:42 -0400

    Removed comments

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,46 +1,9 @@
-// import { useRef /*, forwardRef, useState*/ } from 'react';
-
 const Register = () => {
-  //   const formRef = useRef(null);
-
-  //   const submitForm = () => {
-  //     const registrationForm = formRef.current;
-  //     const form = jsonifyForm(registrationForm);
-
-  //     const postReq = {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify(form),
-  //     };
-
-  //     fetch('https://weak-puce-toad-garb.cyclic.app/', postReq).catch((error) =>
-  //       console.log(error)
-  //     );
-  //     // pantryClient.basket
-  //     //   .update('accounts', form, options)
-  //     //   .then((response) => console.log(response));
-  //   };
-
-  //   const jsonifyForm = (form) => {
-  //     const formKeyNodes = form.querySelectorAll(
-  //       '.field > :nth-child(1):not(.submit)'
-  //     );
-  //     const formValNodes = form.querySelectorAll('.field > :nth-child(2)');
-
-  //     const jsonifiedForm = {};
-  //     for (var i = 0; i < formKeyNodes.length; i++)
-  //       jsonifiedForm[formKeyNodes[i].textContent] = formValNodes[i].value;
-
-  //     return jsonifiedForm;
-  //   };
-
   return (
     <body id='register'>
       <div className='card'>
         <h3>Create an account</h3>
-        <form /*ref={formRef}*/>
+        <form>
           <div className='field'>
             <div>Email</div>
             <input name='Email' type='email' />
@@ -66,12 +29,6 @@ const Register = () => {
             </div>
           </div>
           <div className='field'>
-            {/* <button
-              className='submit'
-              onClick={submitForm}
-            >
-              Create account
-            </button> */}
             <input
               className='submit'
               type='submit'
