ffc886c9b957d56b2cff4d2da79e5cd6f07cd2f3
Ben Bavar <ben.bavar@gmail.com>
2023-11-11 00:25:59 -0500

    Start filling in MyProfile

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,5 +1,17 @@
+import Placeholder from '../images/Portrait_Placeholder.png';
+
 const MyProfile = () => {
-  return <body id='profile'></body>;
+  return (
+    <body id='profile'>
+      <img
+        className='profile-pic'
+        src={Placeholder}
+        alt=''
+        height='300px'
+        width='auto'
+      />
+    </body>
+  );
 };
 
 export default MyProfile;
