acad84e10d9964c25825f4e8ab7da7aa1f8260f4
Ben Bavar <ben.bavar@gmail.com>
2023-12-08 22:43:14 -0500

    Stop using React Hooks, since they lead to excessively many renders of the ProfileMaker component

--- src/components/ProfileMaker.js
+++ src/components/ProfileMaker.js
@@ -1,98 +1,86 @@
 import { Route, Outlet } from 'react-router-dom';
-import { useState } from 'react';
+// import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import Profile from '../pages/Profile';
-let profileRoutes, setProfileRoutes;
+const profileRoutes = [];
 
-const onfulfilled = (
-  res,
-  nameDups,
-  setNameDups,
-  profileRoutes,
-  setProfileRoutes /*nameDupArr, profileRouteArr*/
-) => {
-  // const [nameDups, setNameDups] = nameDupArr;
-  // let [profileRoutes, setProfileRoutes] = profileRouteArr;
+const onfulfilled = async (res) => {
+  const nameDups = new Map();
 
   if (res.data.Count) {
-    let updatedNameDups = new Map();
-    console.log(`nameDups.size = ${nameDups.size}`);
-    if (nameDups.size)
-      for (let [name] of nameDups) {
-        //   console.log(`name = ${name}`);
-        updatedNameDups.set(name, nameDups.get(name));
-      }
-
-    // console.log(`res.data.Items.length = ${res.data.Items.length}`);
-    let i = 1;
     for (let item of res.data.Items) {
       const fullName = `${item['First name']} ${item['Last name']}`;
-
-      console.log(`${i++}: fullName = ${fullName}`);
-
-      if (nameDups.has(fullName)) {
-        console.log('nameDups.has(fullName) returns true');
-        console.log(`nameDups.get(fullName) = ${nameDups.get(fullName)}`);
-        updatedNameDups.set(fullName, nameDups.get(fullName) + 1);
-        console.log(
-          `updatedNameDups.get(fullName) = ${updatedNameDups.get(fullName)}`
-        );
-      } else updatedNameDups.set(fullName, 1);
-
-      // setNameDups(updatedNameDups); // This line causes all the issues
-
-      // console.log(`nameDups.size = ${nameDups.size}`);
+      if (nameDups.has(fullName))
+        nameDups.set(fullName, nameDups.get(fullName) + 1);
+      else nameDups.set(fullName, 1);
     }
-
-    // console.log('AT END OF onfulfilled');
-    // for (let [name] of updatedNameDups) {
-    //   console.log(`name = ${name}`);
-    //   console.log(`updatedNameDups.get(name) = ${updatedNameDups.get(name)}`);
-    // }
-
-    // for (let [name] of updatedNameDups)
-    //   nameDups.set(name, updatedNameDups.get(name));
-    setNameDups(updatedNameDups);
   }
 
-  // for (let [name] of nameDups) {
-  //   // console.log(`name = ${name}`);
-  //   let nameInPath = name.toLowerCase();
-  //   for (let i = 0; i < nameInPath.length; i++)
-  //     if (nameInPath[i] === ' ') nameInPath[i] = '-';
+  for (let [name] of nameDups) {
+    let nameInPath = name.toLowerCase();
+    nameInPath = nameInPath.replaceAll(' ', '-');
 
-  //   if (nameDups.get(name) > 1) nameInPath += `-${crypto.randomUUID()}`;
+    if (nameDups.get(name) > 1) nameInPath += `-${crypto.randomUUID()}`;
 
-  //   setProfileRoutes([
-  //     ...profileRoutes,
-  //     <Route
-  //       path={`/profile/${nameInPath}`}
-  //       element={<Profile nameOfUser={name} />}
-  //     />,
-  //   ]);
-  // }
+    console.log(nameInPath);
+
+    profileRoutes.push(
+      <Route
+        path={`/profile/${nameInPath}`}
+        element={<Profile nameOfUser={name} />}
+      />
+    );
+  }
 };
 
 const ProfileMaker = () => {
   console.log('top of ProfileMaker');
 
-  const [nameDups, setNameDups] = useState(new Map());
-  [profileRoutes, setProfileRoutes] = useState([]);
+  // const [nameDups, setNameDups] = useState(new Map());
+  // [profileRoutes, setProfileRoutes] = useState([]);
+
   // const nameDupArr = [nameDups, setNameDups];
   // const profileRouteArr = [profileRoutes, setProfileRoutes];
 
   axios.get('https://weak-puce-toad-garb.cyclic.app/names-of-users').then(
-    (res) =>
-      onfulfilled(
-        res,
-        nameDups,
-        setNameDups,
-        profileRoutes,
-        setProfileRoutes /*nameDupArr, profileRouteArr*/
-      ),
+    (res) => onfulfilled(res),
     (error) => console.log(error)
   );
+
+  // useEffect(
+  //   () =>
+  //     axios.get('https://weak-puce-toad-garb.cyclic.app/names-of-users').then(
+  //       (res) =>
+  //         onfulfilled(
+  //           res,
+  //           nameDups,
+  //           setNameDups,
+  //           profileRoutes,
+  //           setProfileRoutes /*nameDupArr, profileRouteArr*/
+  //         ),
+  //       (error) => console.log(error)
+  //     ),
+  //   []
+  // );
+
+  // useEffect(
+  //   () =>
+  //     fetch('https://weak-puce-toad-garb.cyclic.app/names-of-users', {
+  //       method: 'GET',
+  //     }).then(
+  //       (res) =>
+  //         onfulfilled(
+  //           res,
+  //           nameDups,
+  //           setNameDups,
+  //           profileRoutes,
+  //           setProfileRoutes /*nameDupArr, profileRouteArr*/
+  //         ),
+  //       (error) => console.log(error)
+  //     ),
+  //   []
+  // );
 
   return <Outlet />;
 };
